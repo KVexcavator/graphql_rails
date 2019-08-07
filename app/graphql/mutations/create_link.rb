e6ff03bb70f010eac207779b318b81b24ed37a18
kv-2 <str_token@mail.ru>
2019-08-07 15:40:58 +0300

    add Link mutation resolver

---
+++ app/graphql/mutations/create_link.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class CreateLink < BaseMutation
+    #arg pass to the resolver method
+    argument :description, String, required: true
+    argument :url, String, required: true
+
+    # return rype from the mutatuin
+    type Types::LinkType
+
+    def resolve (description: nul, url: nul)
+      Link.create!(
+        description: description
+        url: url
+      )
+    end
+  end
+end
