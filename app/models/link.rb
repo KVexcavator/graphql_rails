e34aa6c12a248702cc8d821d352ebdedf876a9e3
kv-2 <str_token@mail.ru>
2019-08-08 17:55:16 +0300

    aplication errors to link create

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :url, presence: true, length: { minimum: 5 }
-  validates :description, presence: true, length: { meinimum: 5 }
-  
+  validates :description, presence: true, length: { minimum: 5 }
+
   has_many :snaps
 end
