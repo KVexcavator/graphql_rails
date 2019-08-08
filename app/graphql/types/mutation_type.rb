c25e582f33af2e8f4ce8f9d7dfcda73c623fa8c3
kv-2 <str_token@mail.ru>
2019-08-08 16:00:47 +0300

    add create_snap to mutation list

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_user, mutation: Mutations::CreateUser
     field :create_link, mutation: Mutations::CreateLink
     field :signin_user, mutation: Mutations::SignInUser
+    field :create_snap, mutation: Mutations::CreateSnap
   end
 end
