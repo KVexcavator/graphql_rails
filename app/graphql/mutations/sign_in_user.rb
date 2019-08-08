64b2c515cfab52cf336e7cb1896df48d99c06bed
kv-2 <str_token@mail.ru>
2019-08-08 12:24:46 +0300

    redo erratum

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -1,25 +1,26 @@
 module Mutations
   class SignInUser < BaseMutation
     null true
-
+  
     argument :email, Types::AuthProviderEmailInput, required: false
-
+  
     field :token, String, null: true
     field :user, Types::UserType, null: true
-
+  
     def resolve(email: nil)
-      # starting validate
+      # start  validate
       return unless email
-
+  
       user = User.find_by email: email[:email]
-      # have a correct user
+  
+      # correct user
       return unless user
       return unless user.authenticate(email[:password])
-      
-      #use RoR's ActiveSupport:: MessageEncriptor to build a token
-      crypt = ActiveSupport::MessageEncriptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
-      token = crypt.encrypt_and_sing("user-id:#{ user.id }")
-
+  
+      # use RoR's ActiveSupport::MessageEncryptor, to  token
+      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+      token = crypt.encrypt_and_sign("user-id:#{ user.id }")
+  
       { user: user, token: token }
     end
   end
