7242fd7f10db7d02e2fdfd90a479d2d261dac4db
kv-2 <str_token@mail.ru>
2019-08-08 12:57:38 +0300

    add test to dign user

---
+++ test/graphql/mutations/dign_in_user_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class Mutations::SignInUserTest < ActiveSupport::TestCase
+
+  def perform(args = {})
+    Mutations::SignInUser.new(object: nil, context: {session: {}}).resolve(args)
+  end
+
+  def create_user
+    User.create!(
+      name: "Test User",
+      email: "email@mail.com",
+      password: '[omitted]'
+    )
+  end
+
+  test 'success' do
+    user = create_user
+
+    result = perform(
+      email:{
+        email: user.email,
+        password: user.password
+      }
+    )
+
+    assert result[:token].present?
+    assert_equal result[:user], user
+  end
+
+  test 'failure because wrong email' do
+    create_user
+    assert_nil perform(email: {email: 'wrong'})
+  end
+
+  test 'failure because wrong password' do
+    user = create_user
+    assert_nil perform(email: {email: user.email, password: 'wrong'})
+  end
+end
