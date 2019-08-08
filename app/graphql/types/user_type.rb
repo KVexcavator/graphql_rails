b7772d4475905499203fa02755af5b661b9fb575
kv-2 <str_token@mail.ru>
2019-08-08 17:21:37 +0300

    ralating user with their snaps

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,8 +1,11 @@
 module Types
   class UserType < BaseObject
     field :id, ID, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :name, String, null: false
-    # only for exemple, peodaction shouldn't lesk user email
+    # only for exemple, prodaction shouldn't  user email
     field :email, String, null: false
+    field :snaps, [Types::SnapType], null: false
+    field :links, [Types::LinkType], null: false
   end
 end
