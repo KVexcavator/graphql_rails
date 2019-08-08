918e6c06489ddfbee3a9a2052d63bb79449ae80e
kv-2 <str_token@mail.ru>
2019-08-08 16:11:50 +0300

    relating links with their snaps

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -4,5 +4,6 @@ module Types
     field :url, String, null: false
     field :description, String, null: false
     field :posted_by, UserType, null: true, method: :user
+    field :snaps, [Types::SnapType], null: false
   end
 end
