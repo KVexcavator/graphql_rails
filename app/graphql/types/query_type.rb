2c3e65f44871768e725be3515187e3c40d7bb9e3
kv-2 <str_token@mail.ru>
2019-08-08 18:31:29 +0300

    add search resolver

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,10 +1,6 @@
 module Types
   class QueryType < Types::BaseObject
     # allLinks resolver
-    field :all_links, [LinkType], null: false
-    def all_links
-      Link.all
-    end
-
+    field :all_links, function: Resolvers::LinksSearch
   end
 end
